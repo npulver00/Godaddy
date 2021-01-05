96abc5af394ce5db2820c8bbc1e0e0b4687593e6
jbnguyen215 <jbnguyen9889@gmail.com>
2021-01-04 19:59:53 -0700

    Created placeholder Page Object Model and cartTest files

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,8 @@
-typescriptmodule.exports = {
+module.exports = {
     preset: "ts-jest",
     testEnvironment: "node",
+    //testPathIgnorePatterns: ["/node_modules/", "tests/pageObjects/"],
+    testPathIgnorePatterns: ["__tests__/pageObjects/"],
+    //setupFilesAfterEnv: ["./jest.setup.js"],
+    testTimeout: 30000,
   };
