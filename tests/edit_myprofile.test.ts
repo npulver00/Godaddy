3d1e04a8bf674ab6c38856983ec3beb8f9a79fe3
bhagatnx <nishan.bhagat@gmail.com>
2021-01-08 19:01:28 -0500

    not working due to godaddy blocking access

--- tests/edit_myprofile.test.ts
+++ tests/edit_myprofile.test.ts
@@ -4,13 +4,13 @@ import { godaddy } from "./pageObjects/nbplaceholder";
 const go = new godaddy();
 //manual testing will be conducted as GoDaddy has blocked access to login portion
 
- /* The blocked out selectors are for my profile (manual testing will be conducted due to issue with blocker*/
-  //qalogo: By = By.xpath("//span[text()='QA']");
-  //accountsetting: By = By.xpath("//a[contains(text(), 'Account Settings')]");
-  //editbutton: By = By.css('[class="btn btn-link btn-sm"]');
-  //organization: By = By.css('[for="organization"]');
-  //editorganization: By = By.css('[name="organization"]');
-  //savebutton: By = By.css('[class="btn btn-primary ux-btn-set-item"]');
+/* The blocked out selectors are for my profile (manual testing will be conducted due to issue with blocker*/
+//qalogo: By = By.xpath("//span[text()='QA']");
+//accountsetting: By = By.xpath("//a[contains(text(), 'Account Settings')]");
+//editbutton: By = By.css('[class="btn btn-link btn-sm"]');
+//organization: By = By.css('[for="organization"]');
+//editorganization: By = By.css('[name="organization"]');
+//savebutton: By = By.css('[class="btn btn-primary ux-btn-set-item"]');
 
 describe("edit profile", () => {
 
@@ -21,22 +21,15 @@ describe("edit profile", () => {
     await go.quit();
   });
 
- // await this.click(this.domainsHomePage)
- // await this.getElement(this.domainsHomePage);
-     
-//await this.click(this.logo)
- // await this.getElement(this.logo);
 
   it("", async () => {
     //jest.setTimeout(15000)
-   //go.openDropdown()
-   //go.selectSignIn()
+    //go.openDropdown()
+    //go.selectSignIn()
 
 
   })
 
-
-//})
 
 
 })
